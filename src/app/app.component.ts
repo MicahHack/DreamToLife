bac4d1d934110f37b0a92f4b9fdc3ca716b05670
MicahHack <Micah.Hack.18@gmail.com>
2024-08-22 16:53:12 +0200

    Completed titlebar, installed primeflex, and completed intro section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TitlebarComponent } from './Components/titlebar/titlebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TitlebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Dream To Life';
 }
